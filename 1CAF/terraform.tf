4ea0f37d3a9a8b4625edc2db022d7a57390d7664
Basant Pandey <basantwelcome2you@gmail.com>
2023-06-12 21:05:25 +0530

    Update Terraform

--- 1CAF/terraform.tf
+++ 1CAF/terraform.tf
@@ -1,3 +1,15 @@
 terraform {
-  
+    required_version = ">= 1.0.0"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = ">= 2.0" 
+        }
+    }
+    backend "azurerm" {
+        resource_group_name   = "terraform-demo"
+        storage_account_name  = "terraformdemoazure "
+        container_name        = "tfstate"
+        key                   = "terraform.tfstate"
+  }
 }
