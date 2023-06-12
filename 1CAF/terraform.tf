d6792596821b165bde337e8e4e758a50bd0f0ae2
Basant Pandey <basantwelcome2you@gmail.com>
2023-06-12 21:09:50 +0530

    update Storage name

--- 1CAF/terraform.tf
+++ 1CAF/terraform.tf
@@ -8,7 +8,7 @@ terraform {
     }
     backend "azurerm" {
         resource_group_name   = "terraform-demo"
-        storage_account_name  = "terraformdemoazure "
+        storage_account_name  = "terraformdemoazure"
         container_name        = "tfstate"
         key                   = "terraform.tfstate"
   }
